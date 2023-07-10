c296f21d31a527ab554a6575bd19c34c1e4c4e61
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-10 16:09:13 +0200

    add migration types, add array, add type contr.

---
+++ app/Http/Controllers/Admin/TypeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Type $type)
+    {
+        //
+    }
+
+    public function edit(Type $type)
+    {
+        //
+    }
+
+    public function update(Request $request, Type $type)
+    {
+        //
+    }
+
+    public function destroy(Type $type)
+    {
+        //
+    }
+}
